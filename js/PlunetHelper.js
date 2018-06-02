aa0485596167eb6b4e8e603f7dce52651bd5f624
TinkerToysCodeManufaktur (Mark6) <tinkertoys.codemanufaktur@gmail.com>
2018-06-02 03:15:22 +0200

    latest

--- js/PlunetHelper.js
+++ js/PlunetHelper.js
@@ -11,81 +11,86 @@ Date.prototype.isDstObserved = function () {
     return this.getTimezoneOffset() < this.stdTimezoneOffset();
 }
 // read settings from local storage
-var addCC = (localStorage["addCC"] == undefined ? true : (localStorage["addCC"] == 'true'));
-var dSTfix = (localStorage["dSTfix"] == undefined ? true : (localStorage["dSTfix"] == 'true'));
-var makeBold = (localStorage["makeBold"] == undefined ? true : (localStorage["makeBold"] == 'true'));
-var makeImportant = (localStorage["makeImportant"] == undefined ? true : (localStorage["makeImportant"] == 'true'));
-var makeHighlighted = (localStorage["makeHighlighted"] == undefined ? true : (localStorage["makeHighlighted"] == 'true'));
-var cleanUp = (localStorage["cleanUp"] == undefined ? true : (localStorage["cleanUp"] == 'true'));
-var swapMQ = (localStorage["swapMQ"] == undefined ? true : (localStorage["swapMQ"] == 'true'));
-var highlightMQ = (localStorage["highlightMQ"] == undefined ? true : (localStorage["highlightMQ"] == 'true'));
-var highColor = (localStorage["highColor"] == undefined ? "#000000" : localStorage["highColor"]);
-var highBColor = (localStorage["highBColor"] == undefined ? "#ffff99" : localStorage["highBColor"]);
-if (addCC) {
-    // look for the CC field and if found then add Coordination
-    var cc = document.querySelector('[id^="ATagBox05CC"]');
-    var ccSelect = cc.getElementsByClassName('selectWrapper');
-    if (ccSelect.length > 0) {
-        ccSelect = ccSelect[0].getElementsByClassName('ng-non-bindable');
-        if (ccSelect.length > 0) {
-            ccSelect = ccSelect[0];
-            ccSelect.innerHTML = '<option id="ATagBox07#' + ccSelect.getAttribute('name') + '#Q29vcmRpbmF0aW9uIFRlYW0gIDxjb29yZGluYXRpb25AYWx0YWdyYW0uY29tPg==" value="Q29vcmRpbmF0aW9uIFRlYW0gIDxjb29yZGluYXRpb25AYWx0YWdyYW0uY29tPg==" selected="selected">coordination@altagram.com</option>';
+chrome.storage.local.get(null, function(settings) {
+    if (!chrome.runtime.error) {
+        //console.log(settings);
+        var addCC = (settings.addCC == undefined ? true : settings.addCC);
+        var dSTfix = (settings.dSTfix == undefined ? true : settings.dSTfix);
+        var makeBold = (settings.makeBold == undefined ? true : settings.makeBold);
+        var makeImportant = (settings.makeImportant == undefined ? true : settings.makeImportant);
+        var makeHighlighted = (settings.makeHighlighted == undefined ? true : settings.makeHighlighted);
+        var cleanUp = (settings.cleanUp == undefined ? true : settings.cleanUp);
+        var swapMQ = (settings.swapMQ == undefined ? true : settings.swapMQ);
+        var highlightMQ = (settings.highlightMQ == undefined ? true : settings.highlightMQ);
+        var highColor = (settings.highColor == undefined ? "#000000" : settings.highColor);
+        var highBColor = (settings.highBColor == undefined ? "#ffff99" : settings.highBColor);
+        if (addCC) {
+            // look for the CC field and if found then add Coordination
+            var cc = document.querySelector('[id^="ATagBox05CC"]');
+            var ccSelect = cc.getElementsByClassName('selectWrapper');
+            if (ccSelect.length > 0) {
+                ccSelect = ccSelect[0].getElementsByClassName('ng-non-bindable');
+                if (ccSelect.length > 0) {
+                    ccSelect = ccSelect[0];
+                    ccSelect.innerHTML = '<option id="ATagBox07#' + ccSelect.getAttribute('name') + '#Q29vcmRpbmF0aW9uIFRlYW0gIDxjb29yZGluYXRpb25AYWx0YWdyYW0uY29tPg==" value="Q29vcmRpbmF0aW9uIFRlYW0gIDxjb29yZGluYXRpb25AYWx0YWdyYW0uY29tPg==" selected="selected">coordination@altagram.com</option>';
+                }
+            }
+            var ccFakeSelect = cc.getElementsByClassName('ellipsis');
+            if (ccFakeSelect.length > 0) {
+                ccFakeSelect = ccFakeSelect[0];
+                ccFakeSelect.innerHTML = '<span title="coordination@altagram.com <coordination@altagram.com>">coordination@altagram.com</span><div class="icon-cross-small deleteButton"></div><div class="icon-cross-small deleteButtonHover"></div>';
+                ccFakeSelect.classList.remove('hinweistext');
+                ccFakeSelect.classList.add('selectedTag');
+            }
         }
-    }
-    var ccFakeSelect = cc.getElementsByClassName('ellipsis');
-    if (ccFakeSelect.length > 0) {
-        ccFakeSelect = ccFakeSelect[0];
-        ccFakeSelect.innerHTML = '<span title="coordination@altagram.com <coordination@altagram.com>">coordination@altagram.com</span><div class="icon-cross-small deleteButton"></div><div class="icon-cross-small deleteButtonHover"></div>';
-        ccFakeSelect.classList.remove('hinweistext');
-        ccFakeSelect.classList.add('selectedTag');
-    }
-}
-// prettyfy/enhance the mail body text
-var tags = document.getElementsByTagName('textarea');
-for (var i = 0; i < tags.length; i++) {
-    if (dSTfix) {
-        // check if DST is on, and if so, fix the "CET" indicator
-        var today = new Date();
-        if (today.isDstObserved()) {
-            if (tags[i].innerHTML.match(/\b(\d{1,2}:\d\d)\sCET\b/i) !== null) {
-                tags[i].innerHTML = tags[i].innerHTML.replace(/\b(\d{1,2}:\d\d)\sCET\b/ig, '$1 CEST');
+        // prettyfy/enhance the mail body text
+        var tags = document.getElementsByTagName('textarea');
+        for (var i = 0; i < tags.length; i++) {
+            if (dSTfix) {
+                // check if DST is on, and if so, fix the "CET" indicator
+                var today = new Date();
+                if (today.isDstObserved()) {
+                    if (tags[i].innerHTML.match(/\b(\d{1,2}:\d\d)\sCET\b/i) !== null) {
+                        tags[i].innerHTML = tags[i].innerHTML.replace(/\b(\d{1,2}:\d\d)\sCET\b/ig, '$1 CEST');
+                    }
+                }
+            }
+            if (swapMQ) { // look if file(s) info precedes memoq project name, and flip them
+                var turned = tags[i].innerHTML.match(/(?![\r\n]\s*?)(file(s)?\s*?(\s*?\(\d+?\))?:\s*?)([^<>]+?)(\s*?)(memoq\s+?project\s*?:\s*?)([^<>]+?)(\s*?[\r\n])/gism);
+                if (turned !== null) {
+                    //tags[i].innerHTML = tags[i].innerHTML.replace(/(?![\r\n]\s*?)(file(s)?\s*?(\s*?\(\d+?\))?:\s*?)([^<>]+?)(\s*?)(memoq\s+?project\s*?:\s*?)([^<>]+?)(\s*?[\r\n])/gism, '$6$7$8$1$4');
+                }
+            }
+            if (highlightMQ) { // look for memoQ project name and file(s) list info, and highlight if found
+                var mqp = tags[i].innerHTML.match(/(?![\r\n]\s*?)(memoq\s+?project\s*?:\s*?)([^<>]+?)(\s*?[\r\n])/is);
+                if (mqp !== null) {
+                    tags[i].innerHTML = tags[i].innerHTML.replace(/(?![\r\n]\s*?)(memoq\s+?project\s*?:\s*?)([^<>]+?)(\s*?[\r\n])/is, '<strong><span style="color:' + highColor + ';background-color:' + highBColor + ';">$1$2</span></strong>$3');
+                }
+                var mqf = tags[i].innerHTML.match(/(?![\r\n]\s*?)(file(s)?\s*?(\s*?\(\d+?\))?:\s*?)([^<>]+?)(\s*?[\r\n][\r\n])/is);
+                if (mqf !== null) {
+                    tags[i].innerHTML = tags[i].innerHTML.replace(/(?![\r\n]\s*?)(file(s)?\s*?(\s*?\(\d+?\))?:\s*?)([^<>]+?)(\s*?[\r\n][\r\n])/is, '<strong><span style="color:' + highColor + ';background-color:' + highBColor + ';>$1$4</span></strong>$5');
+                }
+            }
+            if (cleanUp) { // get rid of unnecessary linebreaks
+                if (tags[i].innerHTML.match(/&lt;div&gt;&lt;span[^&]*?&gt;&lt;\/span&gt;&lt;\/div&gt;/gism) !== null) {
+                    tags[i].innerHTML = tags[i].innerHTML.replace(/&lt;div&gt;&lt;span[^&]*?&gt;&lt;\/span&gt;&lt;\/div&gt;/gism, '');
+                }
+            }
+            if (makeBold) { // look for *bold* shortcuts and format as <strong>bold</strong>
+                if (tags[i].innerHTML.match(/\*([^<>]+?)\*/gism) !== null) {
+                    tags[i].innerHTML = tags[i].innerHTML.replace(/\*([^<>]+?)\*/gism, '<strong>$1</strong>');
+                }
+            }
+            if (makeImportant) { // look for §important§ shortcuts and format as <strong style="color:red;">important</strong>
+                if (tags[i].innerHTML.match(/\§([^<>]+?)\§/gism) !== null) {
+                    tags[i].innerHTML = tags[i].innerHTML.replace(/\§([^<>]+?)\§/gism, '<strong><span style="color:#ff0000;">$1</span></strong>');
+                }
+            }
+            if (makeHighlighted) { // look for #highlighting# shortcuts and format accordingly
+                if (tags[i].innerHTML.match(/\~([^<>]+?)\~/gism) !== null) {
+                    tags[i].innerHTML = tags[i].innerHTML.replace(/\~([^<>]+?)\~/gism, '<span style="color:' + highColor + ';background-color:' + highBColor + ';">$1</span>');
+                }
             }
         }
     }
-    if (swapMQ) { // look if file(s) info precedes memoq project name, and flip them
-        var turned = tags[i].innerHTML.match(/(?![\r\n]\s*?)(file(s)?\s*?(\s*?\(\d+?\))?:\s*?)([^<>]+?)(\s*?)(memoq\s+?project\s*?:\s*?)([^<>]+?)(\s*?[\r\n])/gism);
-        if (turned !== null) {
-            //tags[i].innerHTML = tags[i].innerHTML.replace(/(?![\r\n]\s*?)(file(s)?\s*?(\s*?\(\d+?\))?:\s*?)([^<>]+?)(\s*?)(memoq\s+?project\s*?:\s*?)([^<>]+?)(\s*?[\r\n])/gism, '$6$7$8$1$4');
-        }
-    }
-    if (highlightMQ) { // look for memoQ project name and file(s) list info, and highlight if found
-        var mqp = tags[i].innerHTML.match(/(?![\r\n]\s*?)(memoq\s+?project\s*?:\s*?)([^<>]+?)(\s*?[\r\n])/is);
-        if (mqp !== null) {
-            tags[i].innerHTML = tags[i].innerHTML.replace(/(?![\r\n]\s*?)(memoq\s+?project\s*?:\s*?)([^<>]+?)(\s*?[\r\n])/is, '<strong><span style="color:' + highColor + ';background-color:' + highBColor + ';">$1$2</span></strong>$3');
-        }
-        var mqf = tags[i].innerHTML.match(/(?![\r\n]\s*?)(file(s)?\s*?(\s*?\(\d+?\))?:\s*?)([^<>]+?)(\s*?[\r\n][\r\n])/is);
-        if (mqf !== null) {
-            tags[i].innerHTML = tags[i].innerHTML.replace(/(?![\r\n]\s*?)(file(s)?\s*?(\s*?\(\d+?\))?:\s*?)([^<>]+?)(\s*?[\r\n][\r\n])/is, '<strong><span style="color:' + highColor + ';background-color:' + highBColor + ';>$1$4</span></strong>$5');
-        }
-    }
-    if (cleanUp) { // get rid of unnecessary linebreaks
-        if (tags[i].innerHTML.match(/&lt;div&gt;&lt;span[^&]*?&gt;&lt;\/span&gt;&lt;\/div&gt;/gism) !== null) {
-            tags[i].innerHTML = tags[i].innerHTML.replace(/&lt;div&gt;&lt;span[^&]*?&gt;&lt;\/span&gt;&lt;\/div&gt;/gism, '');
-        }
-    }
-    if (makeBold) { // look for *bold* shortcuts and format as <strong>bold</strong>
-        if (tags[i].innerHTML.match(/\*([^<>]+?)\*/gism) !== null) {
-            tags[i].innerHTML = tags[i].innerHTML.replace(/\*([^<>]+?)\*/gism, '<strong>$1</strong>');
-        }
-    }
-    if (makeImportant) { // look for §important§ shortcuts and format as <strong style="color:red;">important</strong>
-        if (tags[i].innerHTML.match(/\§([^<>]+?)\§/gism) !== null) {
-            tags[i].innerHTML = tags[i].innerHTML.replace(/\§([^<>]+?)\§/gism, '<strong><span style="color:#ff0000;">$1</span></strong>');
-        }
-    }
-    if (makeHighlighted) { // look for #highlighting# shortcuts and format accordingly
-        if (tags[i].innerHTML.match(/\~([^<>]+?)\~/gism) !== null) {
-            tags[i].innerHTML = tags[i].innerHTML.replace(/\~([^<>]+?)\~/gism, '<strong><span style="color:' + highColor + ';background-color:' + highBColor + ';">$1</span></strong>');
-        }
-    }
-}
+});
--- js/options.js
+++ js/options.js
@@ -1,100 +1,89 @@
-var defaultHighColor = "#000000";
-var defaultHighBColor = "#ffff99";
-
 document.addEventListener('DOMContentLoaded', initOptions);
 
 function initOptions() {
 	//var manifestData = chrome.runtime.getManifest();
 	chrome.storage.local.get(null, function(settings) {
-
-
-		
-	})
-	var addCC = localStorage["addCC"];
-	if (addCC == undefined) {
-		addCC = true;
-		localStorage["addCC"] = addCC;
-	} else
-		addCC = (addCC == 'true');
-	document.getElementById("addCC").checked = addCC;
-	var dSTfix = localStorage["dSTfix"];
-	if (dSTfix == undefined) {
-		dSTfix = true;
-		localStorage["dSTfix"] =dSTfix;
-	} else
-		dSTfix = (dSTfix == 'true');
-	document.getElementById("dSTfix").checked = dSTfix;
-	var makeBold = localStorage["makeBold"];
-	if (makeBold == undefined) {
-		makeBold = true;
-		localStorage["makeBold"] = makeBold;
-	} else
-		makeBold = (makeBold == 'true');
-	document.getElementById("makeBold").checked = makeBold;
-	var makeImportant = localStorage["makeImportant"];
-	if (makeImportant == undefined) {
-		makeImportant = true;
-		localStorage["makeImportant"] = makeImportant;
-	} else
-		makeImportant = (makeImportant == 'true');
-	document.getElementById("makeImportant").checked = makeImportant;
-	var makeHighlighted = localStorage["makeHighlighted"];
-	if (makeHighlighted == undefined) {
-		makeHighlighted = true;
-		localStorage["makeHighlighted"] = makeHighlighted;
-	} else
-		makeHighlighted = (makeHighlighted == 'true');
-	document.getElementById("makeHighlighted").checked = makeHighlighted;
-	var cleanUp = localStorage["cleanUp"];
-	if (cleanUp == undefined) {
-		cleanUp = true;
-		localStorage["cleanUp"] = cleanUp;
-	} else
-		cleanUp = (cleanUp == 'true');
-	document.getElementById("cleanUp").checked = cleanUp;
-	var swapMQ = localStorage["swapMQ"];
-	if (swapMQ == undefined) {
-		swapMQ = true;
-		localStorage["swapMQ"] = swapMQ;
-	} else
-		swapMQ = (swapMQ == 'true');
-	document.getElementById("swapMQ").checked = swapMQ;
-	var highlightMQ = localStorage["highlightMQ"];
-	if (highlightMQ == undefined) {
-		highlightMQ = true;
-		localStorage["highlightMQ"] = highlightMQ;
-	} else
-		highlightMQ = (highlightMQ == 'true');
-	document.getElementById("highlightMQ").checked = highlightMQ;
-	var highColor = localStorage["highColor"];
-	var highBColor = localStorage["highBColor"];
-	if ((highColor == undefined) || (highBColor == undefined)) {
-		highColor = defaultHighColor;
-		highBColor = defaultHighBColor;
-		localStorage["highColor"] = highColor;
-		localStorage["highBColor"] = highBColor;
-	}
-	document.getElementById('colorswatch').addEventListener('change', saveColor);
-	var select = document.getElementById("colorswatch");
-	for (var i = 0; i < select.children.length; i++) {
-		var child = select.children[i];
-		if (child.value == highBColor) {
-			child.selected = "true";
-			highColor = child.getAttribute('color');
-			document.getElementById("colorsample").style.color = highColor;
-			document.getElementById("colorsample").style.backgroundColor = highBColor;
-			break;
+		if (!chrome.runtime.error) {
+			//console.log(settings);
+			var addCC = settings.addCC;
+			if (addCC == undefined) {
+				addCC = true;
+				chrome.storage.local.set({"addCC":addCC});
+			}
+			document.getElementById("addCC").checked = addCC;
+			var dSTfix = settings.dSTfix;
+			if (dSTfix == undefined) {
+				dSTfix = true;
+				chrome.storage.local.set({"dSTfix":dSTfix});
+			}
+			document.getElementById("dSTfix").checked = dSTfix;
+			var makeBold = settings.makeBold;
+			if (makeBold == undefined) {
+				makeBold = true;
+				chrome.storage.local.set({"makeBold":makeBold});
+			}
+			document.getElementById("makeBold").checked = makeBold;
+			var makeImportant = settings.makeImportant;
+			if (makeImportant == undefined) {
+				makeImportant = true;
+				chrome.storage.local.set({"makeImportant":makeImportant});
+			}
+			document.getElementById("makeImportant").checked = makeImportant;
+			var makeHighlighted = settings.makeHighlighted;
+			if (makeHighlighted == undefined) {
+				makeHighlighted = true;
+				chrome.storage.local.set({"makeHighlighted":makeHighlighted});
+			}
+			document.getElementById("makeHighlighted").checked = makeHighlighted;
+			var cleanUp = settings.cleanUp;
+			if (cleanUp == undefined) {
+				cleanUp = true;
+				chrome.storage.local.set({"cleanUp":cleanUp});
+			}
+			document.getElementById("cleanUp").checked = cleanUp;
+			var swapMQ = settings.swapMQ;
+			if (swapMQ == undefined) {
+				swapMQ = true;
+				chrome.storage.local.set({"swapMQ":swapMQ});
+			}
+			document.getElementById("swapMQ").checked = swapMQ;
+			var highlightMQ = settings.highlightMQ;
+			if (highlightMQ == undefined) {
+				highlightMQ = true;
+				chrome.storage.local.set({"highlightMQ":highlightMQ});
+			}
+			document.getElementById("highlightMQ").checked = highlightMQ;
+			var highColor = settings.highColor;
+			var highBColor = settings.highBColor;
+			if ((highColor == undefined) || (highBColor == undefined)) {
+				highColor = "#000000";
+				highBColor = "#ffff99";
+				chrome.storage.local.set({"highColor":highColor});
+				chrome.storage.local.set({"highBColor":highBColor});
+			}
+			document.getElementById('colorswatch').addEventListener('change', saveColor);
+			var select = document.getElementById("colorswatch");
+			for (var i = 0; i < select.children.length; i++) {
+				var child = select.children[i];
+				if (child.value == highBColor) {
+					child.selected = "true";
+					highColor = child.getAttribute('color');
+					document.getElementById("colorsample").style.color = highColor;
+					document.getElementById("colorsample").style.backgroundColor = highBColor;
+					break;
+				}
+			}
+			document.getElementById('colorswatch').addEventListener('change', saveColor);
+			var classname = document.getElementsByClassName("options-clickable");
+			Array.from(classname).forEach(function(element) {
+				element.addEventListener('click', saveOption);
+			});
 		}
-	}
-	var classname = document.getElementsByClassName("options-clickable");
-	Array.from(classname).forEach(function(element) {
-		element.addEventListener('click', saveOption);
 	});
-	document.getElementById('resetdefaults').addEventListener('click', resetOptions);
 }
 
 function saveOption() {
-	localStorage[this.id] = document.getElementById(this.id).checked;
+	chrome.storage.local.set({[this.id]:document.getElementById(this.id).checked});
 }
 
 function saveColor() {
@@ -103,20 +92,6 @@ function saveColor() {
 	var highBColor = select.children[select.selectedIndex].value;
 	document.getElementById("colorsample").style.color = highColor;
 	document.getElementById("colorsample").style.backgroundColor = highBColor;
-	localStorage["highColor"] = highColor;
-	localStorage["highBColor"] = highBColor;
-}
-
-function resetOptions() {
-	localStorage.removeItem("addCC");
-	localStorage.removeItem("dSTfix");
-	localStorage.removeItem("makeBold");
-	localStorage.removeItem("makeImportant");
-	localStorage.removeItem("makeHighlighted");
-	localStorage.removeItem("cleanUp");
-	localStorage.removeItem("swapMQ");
-	localStorage.removeItem("highlightMQ");
-	localStorage.removeItem("highColor");
-	localStorage.removeItem("highBColor");
-	location.reload();
+	chrome.storage.local.set({"highColor":highColor});
+	chrome.storage.local.set({"highBColor":highBColor});
 }
